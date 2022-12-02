aeb7b52183636ac74606e332ca328719e36a938f
monajstn <117608819+monajstn@users.noreply.github.com>
2022-12-01 23:18:19 -0500

    Update Final Project_Webpage.php

--- Final Project_Webpage.php
+++ Final Project_Webpage.php
@@ -1,27 +1,38 @@
 <html>
 <head>
-    <br>Explore Phenotypes</br>
+    <style>
+        h1 {text-align: center;}
+        h3 {text-align: center;}
+        h4 {text-align: center;}
+    </style>
 </head>
-    <body>
-        <form method= POST>
-                <label><br>Select a phenotype<br></label>
+    <h1>Exploration of Phenotype</h1>
+    <h3>By: Monica Jesteen, Balsam Mohammad, and Jonathan Perdomo</h3>
+    <h4>BMES 550-900</h4>
+    <br><i>Instructions: Select a phenotype from the dropdown menu below.</i><br>
+    <div></div>
+    <body> 
+        <form method= POST> 
+                <label><br>Select a phenotype:<br></label> 
                 <select name="phenotype">
                     <option value=''>Select...</option>
-                    <option value='P1'>P1</option>
-                    <option value='P2'>P2</option>
-                    <option value='P3'>P3</option>
-                    <option value='P4'>P4</option>
+                    <option value='BENTA disease'>BENTA disease</option>
+                    <option value='Kleefstra syndrome'>Kleefstra syndrome</option>
+                    <option value='Kleefstra syndrome 2'>Kleefstra syndrome 2</option>
+                    <option value='anemia'>anemia</option>
+                    <option value='anxiety'>anxiety</option>
                 </select>
         <input type='submit' name='Sumbit' value='Submit'/>
         </form>
-        <?php
-            $checkp = $_POST["phenotype"];
-            echo $checkp
-        ?>
-        <?php $histo= exec("python histogram.py ".$_POST["phenotype"]; $echo $histo); ?>
-        <?php $subtabs = exec("python sumtab.py ".$_POST["phenotype"]); echo $subtabs?>
-        <?php $ivg = exec("python igvgraph.py ".$_POST["phenotype"]); echo $ivg?>
+        <?php $argvari = $_POST["phenotype"]; echo $argvari; echo " a is " .is_string($argvari)."<br>"?> <?php #this is to just check the dropdown ment ?>
+
+        <?php #display the resulting graphs and summary tables. Update the path according to the your computer. ?>
+        <?php $histo = escapeshellcmd('python C:\cygwin64\home\marya\Dropbox\Dropbox\bmes550.MonicaJesteen.mj486\web\Final Project\histo.py "'.$argvari.'"'); $outputh =shell_exec($histo); echo $outputh; ?>
+        <?php $sumtabs = escapeshellcmd('python C:\cygwin64\home\marya\Dropbox\Dropbox\bmes550.MonicaJesteen.mj486\web\Final Project\sumtab.py "'.$argvari.'"'); $outputs =shell_exec($sumtabs); echo $outputs; ?>
+        <?php $ivg = escapeshellcmd('python C:\cygwin64\home\marya\Dropbox\Dropbox\bmes550.MonicaJesteen.mj486\web\Final Project\ivg.py "'.$argvari.'"'); $outputi =shell_exec($ivg); echo $outputi;?>
+       
     </body>
+</html>
 </html>
 
 
