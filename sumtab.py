2efc09fa690ab899e917d217300bc4bd786bdaf9
monajstn <117608819+monajstn@users.noreply.github.com>
2022-12-04 11:47:57 -0500

    Update sumtab.py

--- sumtab.py
+++ sumtab.py
@@ -6,14 +6,19 @@ def sumtab (phenotype):
     import sys
     import pandas as pd
     
+    if os.path.isfile('summary_table.csv'): #check if file already exists. If it does, then if statement 
+        # removes it
+        os.remove('summary_table.csv')
+
     # connect the sqlite and perform an query on sqlite database file. Replace URL with URL with sqlite database
-    conn = sqlite3.connect("C:\cygwin64\home\marya\Dropbox\Dropbox\bmes550.MonicaJesteen.mj486\web\Final Project\sv_phenotypes.sqlite")
+    conn = sqlite3.connect("sv_phenotypes.sqlite")
     cur = conn.cursor()
-    cur.execute('SELECT CHROM, SV_START, SV_END, PHENOTYPE FROM sv_phenotypes WHERE phenotype=?', phenotype) # filters through the databased according to the input phenotype from the php.
+    cur.execute('SELECT CHROM, SV_START, SV_END, PHENOTYPE FROM sv_phenotypes WHERE phenotype=?', [phenotype]) # filters through the databased according to the input phenotype from the php.
     filter = cur.fetchall() # fetch the filtered database
-# converts to the dataframe
+    # converts to the dataframe
     data_frame = pd.DataFrame(filter, columns = ['Chromosome', 'Start', 'End', 'Phenotype'])
-    print(data_frame) # print the dataframe from the php 
+    print(data_frame) # print the dataframe from the php. REMOVE LATER
+    data_frame.to_csv('summary_table.csv')
 
     cur.commit()
     
