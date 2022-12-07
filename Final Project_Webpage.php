ef3b316998d8b10c106a00651b547be7982b14db
jonperdomo <jonperdomodb@gmail.com>
2022-12-07 17:29:10 -0500

    BUG: Fix start-up bug

--- Final Project_Webpage.php
+++ Final Project_Webpage.php
@@ -20,60 +20,59 @@
 		require "php/getPhenotypesList.php";
     ?>
 
-    <body>	
+    <body>
 
 		<!-- Create the phenotype drop-down -->
 		<?php
 			// Query for unique chromosomes and unique phenotypes from sqlite database.
 			$pheno_unique = getPhenotypesFromDB('sv_phenotypes.sqlite');
 		?>
-		
+
 		<form method=post>
 			<label><br>Select a phenotype:<br></label>
 			<select name='phenotype'>
 				<option selected='selected'>Select...</option>
-				<?php 
+				<?php
 					foreach($pheno_unique as $item_phenotype){
 						echo "<option value='$item_phenotype'>$item_phenotype</option>";
 					}
-				?>				
+				?>
 			</select>
 		<input type='submit' name='Submit' value='Submit'/>
 		</form>
-		
+
 		<!-- On phenotype selection -->
 		<?php
-		// Show the phenotype
-		$selected_phen = $_POST["phenotype"];
-		echo "Selected phenotype is: " . $selected_phen;
-		echo "<br>";
-
-		// Get the chromosomes
-		$db_chrom = new SQLite3('sv_phenotypes.sqlite');
-		$query_str = "SELECT DISTINCT CHROM FROM SV_PHENOTYPES WHERE PHENOTYPE = '$selected_phen'";
-		$chroms = $db_chrom->query($query_str);
-		$chroms_arr = array();
-		while ($chrom_row = $chroms->fetchArray(SQLITE3_BOTH)) {
-			$chrom_value = $chrom_row[0];
-			$chrom_str = strval($chrom_value);  // Convert to string
-			$chroms_arr[] = $chrom_str;
-		}
-
-		// Creeate the table
+		// Create the table
 		if(isset($_POST["phenotype"]) and (strcmp($_POST["phenotype"], "Select...") !== 0)){
-		   // Show the table			   
+            // Show the phenotype
+            $selected_phen = $_POST["phenotype"];
+            echo "Selected phenotype is: " . $selected_phen;
+            echo "<br>";
+
+            // Get the chromosomes
+            $db_chrom = new SQLite3('sv_phenotypes.sqlite');
+            $query_str = "SELECT DISTINCT CHROM FROM SV_PHENOTYPES WHERE PHENOTYPE = '$selected_phen'";
+            $chroms = $db_chrom->query($query_str);
+            $chroms_arr = array();
+            while ($chrom_row = $chroms->fetchArray(SQLITE3_BOTH)) {
+                $chrom_value = $chrom_row[0];
+                $chrom_str = strval($chrom_value);  // Convert to string
+                $chroms_arr[] = $chrom_str;
+            }
+
+		   // Show the table
 		   $data_query_str = "SELECT CHROM, SV_START, SV_END, PHENOTYPE FROM SV_PHENOTYPES WHERE PHENOTYPE = '$selected_phen'";
 		   $sv_data = $db_chrom->query($data_query_str);
 		   
 		   // Create the table from the data
 		   createSummaryTable($sv_data);
 		} else {
-		   echo "None selected.";
+		   echo "None selected.<br>";
 		}
 		?>
 		
 	<!-- Show the chromsome drop-down -->
-	<label>Histogram of Phenotype Location </label>
 	<script>
 	function selectionchanged(){
 		// Format the chromosome value
